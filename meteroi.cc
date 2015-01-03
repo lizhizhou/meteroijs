dbb5b085523a4a524e4dd25369f57627c3304191
lizhizhou <lzz@meteroi.com>
2015-01-02 18:55:20 -0800

    initial working version for call c function

--- meteroi.cc
+++ meteroi.cc
@@ -13,7 +13,32 @@ Handle<Value> fun_name##_agent(const Arguments& args) {return agent(#fun_name, a
 #define GET_FUNCTION_HANDLE(fun_name) \
 fun_name##_agent
 #define DEFINE_FUNCTION(ret,function,arg) \
-    {#ret,#function,arg, (void*)function, GET_FUNCTION_HANDLE(function)},
+{#ret,#function,#arg, (void*)function, GET_FUNCTION_HANDLE(function)}
+
+typedef struct table_t {
+	string return_type;
+	string name;
+	string arg_list;
+	void* addr;
+	Handle<Value> (*call)(const Arguments&);
+} fun_table;
+
+fun_table TABLE_NULL = { "", "", "", NULL, NULL };
+
+extern fun_table table[];
+
+int parser_func_arg_count(string arg_list) {
+	int i = 1;
+	const char* arg = arg_list.c_str();
+	if (arg_list.length() == 0)
+		return 0;
+	while (*arg != '\0') {
+		if (*arg == ',')
+			i++;
+		arg++;
+	}
+	return i;
+}
 
 #define REGISTER int
 #define MAX_ARGC 8
@@ -29,102 +54,113 @@ fun_name##_agent
 #define ARG8 ARG7, ARG(7)
 typedef int (*FUNC0)(void);
 typedef int (*FUNC1)(REGISTER);
-typedef int (*FUNC2)(REGISTER,REGISTER);
-typedef int (*FUNC3)(REGISTER,REGISTER,REGISTER);
-typedef int (*FUNC4)(REGISTER,REGISTER,REGISTER,REGISTER);
-typedef int (*FUNC5)(REGISTER,REGISTER,REGISTER,REGISTER,
-                     REGISTER);
-typedef int (*FUNC6)(REGISTER,REGISTER,REGISTER,REGISTER,
-                     REGISTER,REGISTER);
-typedef int (*FUNC7)(REGISTER,REGISTER,REGISTER,REGISTER,
-                     REGISTER,REGISTER,REGISTER);
-typedef int (*FUNC8)(REGISTER,REGISTER,REGISTER,REGISTER,
-                     REGISTER,REGISTER,REGISTER,REGISTER);
+typedef int (*FUNC2)(REGISTER, REGISTER);
+typedef int (*FUNC3)(REGISTER, REGISTER, REGISTER);
+typedef int (*FUNC4)(REGISTER, REGISTER, REGISTER, REGISTER);
+typedef int (*FUNC5)(REGISTER, REGISTER, REGISTER, REGISTER, REGISTER);
+typedef int (*FUNC6)(REGISTER, REGISTER, REGISTER, REGISTER, REGISTER,
+		REGISTER);
+typedef int (*FUNC7)(REGISTER, REGISTER, REGISTER, REGISTER, REGISTER, REGISTER,
+		REGISTER);
+typedef int (*FUNC8)(REGISTER, REGISTER, REGISTER, REGISTER, REGISTER, REGISTER,
+		REGISTER, REGISTER);
 
-Handle<Value> agent(string name, const Arguments& args) 
-{
-  HandleScope scope;
-  if (name == "led") {
-    void* func_address;
-    int   func_arg;
-    ulong argc = 10;
-    REGISTER ret;
-    REGISTER arg[MAX_ARGC];
-//    func_address = get_func_address(name);
-//    func_arg     = (int)get_func_argc_by_name(name);
-        switch(argc) {
-            case 0: ret = ((FUNC0)func_address)(ARG0); break;
-            case 1: ret = ((FUNC1)func_address)(ARG1); break;
-            case 2: ret = ((FUNC2)func_address)(ARG2); break;
-            case 3: ret = ((FUNC3)func_address)(ARG3); break;
-            case 4: ret = ((FUNC4)func_address)(ARG4); break;
-            case 5: ret = ((FUNC5)func_address)(ARG5); break;
-            case 6: ret = ((FUNC6)func_address)(ARG6); break;
-            case 7: ret = ((FUNC7)func_address)(ARG7); break;
-            case 8: ret = ((FUNC8)func_address)(ARG8); break;
-            default: break;
-        }
-	  return scope.Close(Undefined());
+Handle<Value> agent(string name, const Arguments& args) {
+	HandleScope scope;
+	fun_table* table_p = table;
+	void* func_address = NULL;
+	int argc;
+	REGISTER ret = 0;
+	REGISTER arg[MAX_ARGC];
 
-  } else {
+	while (table_p != &TABLE_NULL) {
+		if (table_p->name == name) {
+			func_address = table_p->addr;
+			argc = parser_func_arg_count(table_p->arg_list);
+		}
+		table_p++;
+	}
 
-	  if (args.Length() < 2) {
-	    ThrowException(Exception::TypeError(String::New("Wrong number of arguments")));
-	    return scope.Close(Undefined());
-	  }
+	if (func_address == NULL) {
+		ThrowException(Exception::TypeError(String::New("Wrong method")));
+		return scope.Close(Undefined());
+	}
 
-	  if (!args[0]->IsNumber() || !args[1]->IsNumber()) {
-	    ThrowException(Exception::TypeError(String::New("Wrong arguments")));
-	    return scope.Close(Undefined());
-	  }
+	if (args.Length() != argc) {
+		ThrowException(
+				Exception::TypeError(String::New("Wrong number of arguments")));
+		return scope.Close(Undefined());
+	}
+    //todo function type check
+	//	if (!args[0]->IsNumber() || !args[1]->IsNumber()) {
+	//		ThrowException(Exception::TypeError(String::New("Wrong arguments")));
+	//		return scope.Close(Undefined());
+	//	}
 
-	  Local<Number> num = Number::New(args[0]->NumberValue() +
-	      args[1]->NumberValue());
-	  return scope.Close(num);
-  }
+	for(int i=0; i< argc;i++)
+		arg[i] = args[i]->NumberValue();
+
+	switch (argc) {
+	case 0:
+		ret = ((FUNC0) func_address)(ARG0);
+		break;
+	case 1:
+		ret = ((FUNC1) func_address)(ARG1);
+		break;
+	case 2:
+		ret = ((FUNC2) func_address)(ARG2);
+		break;
+	case 3:
+		ret = ((FUNC3) func_address)(ARG3);
+		break;
+	case 4:
+		ret = ((FUNC4) func_address)(ARG4);
+		break;
+	case 5:
+		ret = ((FUNC5) func_address)(ARG5);
+		break;
+	case 6:
+		ret = ((FUNC6) func_address)(ARG6);
+		break;
+	case 7:
+		ret = ((FUNC7) func_address)(ARG7);
+		break;
+	case 8:
+		ret = ((FUNC8) func_address)(ARG8);
+		break;
+	default:
+		break;
+	}
+
+	Local < Number > num = Number::New(ret);
+	return scope.Close(num);
 }
 
-char add(char a,char b)
-{
-
+char add(char a, char b) {
+	return a + b;
 }
 
-void led(char a,char b)
-{
+void led(char a, char b, char c, char d) {
 
 }
 
 DEFINE_FUNCTION_HANDLE(add);
 DEFINE_FUNCTION_HANDLE(led);
-
-typedef struct table_t 
-{
-    string return_type;
-    string name;
-    string arg_list;
-    void* addr;
-    Handle<Value> (*call)(const Arguments&);
-}fun_table;
-
-fun_table TABLE_NULL = 
-{"", "", "", NULL, NULL};
-
-fun_table table[] =
-{
-DEFINE_FUNCTION(char,add,"char a, char b")
-DEFINE_FUNCTION(void,led,"char a, char b")
+fun_table table[] = {
+DEFINE_FUNCTION(char,add,(char a, char b)),
+DEFINE_FUNCTION(void,led,(char a, char b, char c, char d)),
 TABLE_NULL
 };
 //todo map<string, void*> fun_table;
 
 void Init(Handle<Object> exports) {
-  int i = 0;
-  fun_table* table_p = table;
-  while(table_p != &TABLE_NULL) {
-    exports->Set(String::NewSymbol(table_p->name.c_str()),
-        FunctionTemplate::New(table_p->call)->GetFunction());
-    table_p++;
-  }
+	int i = 0;
+	fun_table* table_p = table;
+	while (table_p != &TABLE_NULL) {
+		exports->Set(String::NewSymbol(table_p->name.c_str()),
+				FunctionTemplate::New(table_p->call)->GetFunction());
+		table_p++;
+	}
 }
 
 NODE_MODULE(meteroi, Init)
